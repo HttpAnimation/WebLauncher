dfd20c6c07235274f595ed745ec04674213e64bc
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-11-29 15:33:49 -0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,25 @@
+import os
+
+def create_desktop_file(url, name):
+    desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+    desktop_file_path = os.path.join(desktop_path, f'{name}.desktop')
+
+    desktop_file_content = f"""[Desktop Entry]
+Name={name}
+Exec=xdg-open {url}
+Type=Application
+"""
+
+    with open(desktop_file_path, 'w') as desktop_file:
+        desktop_file.write(desktop_file_content)
+
+    print(f"Desktop file created at: {desktop_file_path}")
+
+def main():
+    url = input("Enter the URL: ")
+    name = input("Enter the name for the desktop icon: ")
+
+    create_desktop_file(url, name)
+
+if __name__ == "__main__":
+    main()
