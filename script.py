80bce647400f65f5ffe91a235c91e424551f035b
HttpAnimations <97435656+HttpAnimation@users.noreply.github.com>
2023-11-29 15:39:10 -0700

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 import os
+import argparse
 
-def create_desktop_file(url, name):
-    desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-    desktop_file_path = os.path.join(desktop_path, f'{name}.desktop')
+def create_desktop_file(url, name, path):
+    desktop_file_path = os.path.join(path, f'{name}.desktop')
 
     desktop_file_content = f"""[Desktop Entry]
 Name={name}
@@ -16,10 +16,22 @@ Type=Application
     print(f"Desktop file created at: {desktop_file_path}")
 
 def main():
-    url = input("Enter the URL: ")
-    name = input("Enter the name for the desktop icon, this is not a photo: ")
+    parser = argparse.ArgumentParser(description="WebLauncher - Create a desktop icon to open a specified URL.")
+    parser.add_argument("url", help="The URL to open")
+    parser.add_argument("name", help="The name for the desktop icon")
+    parser.add_argument("-p", "--path", help="Specify a custom path for the desktop file")
+    parser.add_argument("-h", "--home", action="store_true", help="Put the desktop file on the desktop where the terminal is opened")
 
-    create_desktop_file(url, name)
+    args = parser.parse_args()
+
+    if args.home:
+        path = os.path.join(os.path.expanduser('~'), 'Desktop')
+    elif args.path:
+        path = args.path
+    else:
+        path = os.path.abspath(os.path.curdir)
+
+    create_desktop_file(args.url, args.name, path)
 
 if __name__ == "__main__":
     main()
